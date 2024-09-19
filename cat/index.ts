63a0d67cdeeecae192e481137250d04150a5f06e
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 10:52:19 +0200

    Saved in TypeScript Playground at 2024-09-19T08:52:18.810Z

--- cat/index.ts
+++ cat/index.ts
@@ -8,10 +8,13 @@ function setup() {
     triangle(250, 270, 270, 260, 230, 260,)
     fill("lightgray")
     triangle(170, 60, 200, 125, 150, 160)
+    triangle(320, 60, 300, 125, 350, 160)
 
     fill("black")
     circle(200, 230, 30)
     circle(300, 230, 30,)
+
+    line(300, 260, 250, 380)
 
     
 
