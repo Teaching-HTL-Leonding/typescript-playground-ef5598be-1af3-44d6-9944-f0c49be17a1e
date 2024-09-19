adb1b0e5c372e72619b07275b2f140cf2c29415c
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 10:42:12 +0200

    Saved in TypeScript Playground at 2024-09-19T08:42:11.709Z

--- cat/index.ts
+++ cat/index.ts
@@ -3,7 +3,17 @@ function setup() {
     background("skyblue")
     fill("lightgray")
     circle(250, 250, 250)
-    
-fill("pink")
+
+    fill("pink")
     triangle(250, 270, 270, 260, 230, 260,)
+    fill("lightgray")
+    triangle(170, 60, 200, 125, 150, 160)
+
+    fill("black")
+    circle(200, 230, 30)
+    circle(300, 230, 30,)
+
+    
+
+    
 }
