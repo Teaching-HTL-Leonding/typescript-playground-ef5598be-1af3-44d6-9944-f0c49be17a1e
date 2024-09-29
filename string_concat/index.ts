5644b398f0696ea920e6a5066431712bf693e3a7
Rainer Stropek <rainer@timecockpit.com>
2024-09-29 13:03:52 +0200

    Saved in TypeScript Playground at 2024-09-29T11:03:51.702Z

--- string_concat/index.ts
+++ string_concat/index.ts
@@ -1,27 +1,29 @@
-let clicks: string[] = []; 
+let clicks: string[] = []
 
 function setup() {
-  createCanvas(, 350); 
-  textSize(20); 
+  createCanvas(530, 530)
+  textSize(20)
 }
 
 function draw() {
-  background("lightblue"); 
-  let x = 10, y = 30; 
+  background("lightblue")
+  let x = 10, y = 30
 
   for (let i = 0; i < clicks.length; i++) {
-    if (i > 0 && i % 5 === 0) { 
+    if (i > 0 && i % 5 === 0) {
       y += 30;
-      x = 10;
+      x = 10
     }
-    text(clicks[i], x, y); 
-    x += textWidth(clicks[i]) + 10; 
+    text(clicks[i], x, y)
+    x += textWidth(clicks[i]) + 10
   }
 }
 
 function mouseClicked() {
-  clicks.push(`(${mouseX}, ${mouseY})`); 
+  clicks.push(`(${mouseX}, ${mouseY})`)
   fill("magenta");
   noStroke();
-  circle(mouseX, mouseY, 15);
+  circle(mouseX, mouseY, 16)
 }
+
+
