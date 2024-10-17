f3ad685848aa5910ad6218f51cdc862326c6112c
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 09:48:26 +0200

    Saved in TypeScript Playground at 2024-10-17T07:48:25.622Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -1,4 +1,6 @@
+
 function setup() {
+
   const num = Math.floor(random(0, 4096));
   const lastDigit1 = num % 16
   const numwithoutlastdigit1 = Math.floor(num / 16)
@@ -6,59 +8,71 @@ function setup() {
   const numwithoutlastdigit2 = Math.floor(numwithoutlastdigit1 / 16)
   const lastDigit3 = numwithoutlastdigit2 % 16
   const numwithoutlastdigit3 = Math.floor(numwithoutlastdigit2 / 16)
-  const lastDigit4 = numwithoutlastdigit3 % 16
-  const numwithoutlastdigit4 = Math.floor(numwithoutlastdigit3 / 16)
-  const lastDigit5 = numwithoutlastdigit4 % 16
-  const numwithoutlastdigit5 = Math.floor(numwithoutlastdigit4 / 16)
-  const lastDigit6 = numwithoutlastdigit5 % 16
-  const numwithoutlastdigit6 = Math.floor(numwithoutlastdigit5 / 16)
 
-  createCanvas(800, 600)
-  background("black")
+  createCanvas(600, 600);
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+
+  background("black");
+  noStroke();
+  text(num, width / 2, height - 20);
+
   fill("yellow");
   strokeWeight(2);
   textAlign(CENTER, CENTER);
   textSize(30);
-  text(`${Math.floor(num / 16 * 16) % 16}`, width / 2.15 + width / 18, height / 3 + height / 6);
-  text(`${Math.floor(num / 16) % 16}`, width / 1.65 + width / 18, height / 3 + height / 6);
-  text(`${num % 16}`, width / 1.35 + width / 18, height / 3 + height / 6);
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  text(num, width / 2, height - 20);
-
-  if (lastDigit1 === 10)
-    text("A", width / 2.15 + width / 18, height / 3 + height / 6)
-    fill("black")
-    stroke("yellow")
+  noFill()
+stroke("yellow")
+  rect(width / 3, height / 3, width / 9, height / 3)
   rect(width / 2.15, height / 3, width / 9, height / 3)
-  if (lastDigit1 === 11)
-  fill("black")
-  stroke("yellow")
-    text("B", width / 2.15 + width / 18, height / 3 + height / 6)
-    fill("black")
-    stroke("yellow")
-  rect(width / 2.15, height / 3, width / 9, height / 3)
-  if (lastDigit1 === 12)
-    text("C", width / 2.15 + width / 18, height / 3 + height / 6)
-    fill("black")
-    stroke("yellow")
-  rect(width / 2.15, height / 3, width / 9, height / 3)
-  if (lastDigit1 ===13)
-  text("D",width / 2.15, height / 3, width / 9, height / 3)
-  if
+  rect(width / 1.68, height / 3, width / 9, height / 3)
+
+  if (lastDigit3 === 10) {
+    text("A", width / 3, height / 3, width / 9, height / 3)
+  } else if (lastDigit3 === 11) {
+    text("B", width / 3, height / 3, width / 9, height / 3)
+  } else if (lastDigit3 === 12) {
+    text("C", width / 3, height / 3, width / 9, height / 3)
+  } else if (lastDigit3 === 13) {
+    text("D", width / 3, height / 3, width / 9, height / 3)
+  } else if (lastDigit3 === 14) {
+    text("E", width / 3, height / 3, width / 9, height / 3)
+  } else if (lastDigit3 === 15) {
+    text("F", width / 3, height / 3, width / 9, height / 3)
+  } else {
+    text(`${lastDigit3}`,width / 3, height / 3, width / 9, height / 3) 
+  }
+  
+  if (lastDigit1 === 10) {
+    text("A", width / 2.15, height / 3, width / 9, height / 3)
+  } else if (lastDigit1 === 11) {
+    text("B",width / 2.15, height / 3, width / 9, height / 3)
+  } else if (lastDigit1 === 12) {
+    text("C", width / 2.15, height / 3, width / 9, height / 3)
+  } else if (lastDigit1 === 13) {
+    text("D", width / 2.15, height / 3, width / 9, height / 3)
+  } else if (lastDigit1 === 14) {
+    text("E", width / 2.15, height / 3, width / 9, height / 3)
+  } else if (lastDigit1 === 15) {
+    text("F", width / 2.15, height / 3, width / 9, height / 3)
+  } else {
+    text(`${lastDigit1}`, width / 2.15, height / 3, width / 9, height / 3)
+  }
 
 
-
-
-
-  stroke("yellow");
-  noFill();
-  strokeWeight(2);
-
-  rect(width / 2.15, height / 3, width / 9, height / 3);
-  rect(width / 1.68, height / 3, width / 9, height / 3);
-  rect(width / 1.38, height / 3, width / 9, height / 3);
-
-}
+if (lastDigit2 === 10) {
+    text("A", width / 1.68, height / 3, width / 9, height / 3)
+  } else if (lastDigit2 === 11) {
+    text("B",width / 1.68, height / 3, width / 9, height / 3)
+  } else if (lastDigit2 === 12) {
+    text("C", width / 1.68, height / 3, width / 9, height / 3)
+  } else if (lastDigit2 === 13) {
+    text("D", width / 1.68, height / 3, width / 9, height / 3)
+  } else if (lastDigit2 === 14) {
+    text("E", width / 1.68, height / 3, width / 9, height / 3)
+  } else if (lastDigit2 === 15) {
+    text("F", width / 1.68, height / 3, width / 9, height / 3)
+  } else {
+    text(`${lastDigit2}`, width / 1.68, height / 3, width / 9, height / 3)
+  }
+  }
