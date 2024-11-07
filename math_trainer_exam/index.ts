9346e70231bf77370b4bbbd76e0c83d939b2155f
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 09:45:28 +0100

    Saved in TypeScript Playground at 2024-11-07T08:45:27.736Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -1,25 +1,78 @@
 
 let operator1 = 0
 let operator2 = 0
-let operator: string = ""
+let operator = 0
+let message = ""
+let solution = 0
+let randomnum1 = 0
+let randomnum2 = 0
+let randomnum3 = 0
+let answer = 0
 function setup() {
     createCanvas(400, 400);
     background("black");
     operator2 = Math.floor(random(1, 101))
     operator1 = Math.floor(random(1, 101))
+    operator = Math.floor(random(0, 3))
+    randomnum1 = Math.floor(random(1, 1000))
+    randomnum2 = Math.floor(random(1, 1000))
+    let randomnum3 = Math.floor(random(1, 1000))
+    answer =Math.floor(random(0, 3))
 
-    textSize(55)
+
+    fill("yellow")
+    switch (operator) {
+        case 0:
+            message = "+"
+            solution = operator1 + operator2
+            break;
+
+        case 1:
+            message = "*"
+            solution = operator1 * operator2
+            break;
+        case 2:
+            message = "-"
+            solution = operator1 - operator2
+            break;
+    }
+
+      switch(answer){
+    
+    case 0:
+    randomnum3 = solution
+    break;
+
+    case 1:
+    randomnum2 = solution
+    break;
+
+    case 2:
+    randomnum3 = solution
+    break;
+      }
+    textSize(40)
     stroke("yellow")
     textAlign(RIGHT)
-    text(`${operator1}`, width / 3, height / 4)
+    text(`${operator1}`, width / 2.6, height / 4)
     textAlign(LEFT)
-    text(`${operator2}`, width / 1.8, height / 4)
+    text(`${operator2}`, width / 1.7, height / 4)
+    textAlign(CENTER)
+    text(`${message}`, width / 2, height / 4)
+      textAlign(RIGHT)
+    text(`${randomnum1}`, width / 3, height / 2)
+    textAlign(CENTER)
+    text(`${randomnum2}`, width / 2, height / 2)
+    textAlign(LEFT)
+    text(`${randomnum3}`, width / 1.6, height / 2)
+    
 
-    if(operator===1)
 
     // mouseClicked function checks if the user clicked on the correct answer
     function mouseClicked() {
-        // <<< TODO: Add your code here
+        if (mouseX < width /10 * 4 && mouseX > width /12 * 3 && mouseY < height/3*2)
+        }
+
 
 
     }
