a1f99ebd8349c5daaa5196facd1e645bccc7a580
Rainer Stropek <rainer@timecockpit.com>
2024-11-28 09:43:11 +0100

    Saved in TypeScript Playground at 2024-11-28T08:43:10.679Z

---
+++ triangles/index.ts
@@ -0,0 +1,24 @@
+function setup() {
+    const SIZE = 400;
+    const SIDE_LENGTH = 50;
+
+    const HEIGHT = SIDE_LENGTH * Math.sqrt(3) / 2;
+
+    createCanvas(SIZE, HEIGHT * 9);
+    background("black");
+
+    strokeWeight(1);
+    stroke("yellow");
+    noFill();
+
+for(let y = 0; y <= SIZE; y+= 0)
+push()
+
+for(let x= 0; x <= SIZE; x+= 0){
+triangle(0, SIDE_LENGTH, HEIGHT, SIDE_LENGTH*0, HEIGHT*0, 0)
+translate(SIDE_LENGTH, 0)
+}
+pop()
+translate(0, HEIGHT);
+
+}
